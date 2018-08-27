9b818b56206b1cf2b4c93a50eb1386b1ea84ad95
ALEKSANDAR VUKASINOVIC <alekvuka@gmail.com>
2018-08-27 23:31:07 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,17 +1,29 @@
 def starts_with_a_vowel?(word)
-
+  if word.match(/^[aeiouAEIOU]/)
+    return true
+  else
+    return false
+  end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
+
+  count_array = text.scan(/\bun\w+ing/)
+  return count_array
 
 end
 
 def words_five_letters_long(text)
 
+  word_array = text.scan(/\b\w{5}\b/)
+  return word_array
+
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
 
+  word_array = text.scan(/\b[A-Z]\w+[!]/)
+  return word_array
 end
 
 def valid_phone_number?(phone)
